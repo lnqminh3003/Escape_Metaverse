226063f619adf711ab35dfd225c6ff0e46f48518
Lê Nguyễn Quang Minh <lnqminh3003@gmail.com>
2022-03-14 08:25:25 +0700

    Add elevator

--- Escape Metaverse/Assets/Scripts/Player/CollisionWall.cs
+++ Escape Metaverse/Assets/Scripts/Player/CollisionWall.cs
@@ -13,7 +13,7 @@ public class CollisionWall : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "Wall" || other.tag =="Decor" || other.tag == "Enemy" || other.tag == "Trap")
+        if (other.tag == "Wall" || other.tag =="Decor" || other.tag == "Enemy" )
         {
             controlMove.ChangeSpeed(0);
             controlMove.ChangeVectorTmp(transform.forward.normalized);
@@ -22,7 +22,7 @@ public class CollisionWall : MonoBehaviour
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.tag == "Wall" || other.tag == "Decor" || other.tag == "Enemy" || other.tag == "Trap")
+        if (other.tag == "Wall" || other.tag == "Decor" || other.tag == "Enemy" )
         {
             controlMove.ChangeSpeed(controlMove.GetSpeed());
             controlMove.ChangeVectorTmp(transform.forward.normalized);
